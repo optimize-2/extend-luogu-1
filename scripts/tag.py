a7ebae6c86d33bb81261b85080b7cfc45b76f666
optimize-2 <55306215+optimize-2@users.noreply.github.com>
2023-08-23 20:28:09 +0800

    傻逼

---
+++ scripts/tag.py
@@ -0,0 +1,8 @@
+import json
+import os
+import re
+
+rb = re.split("^[^\n]+\n\n", json.loads(os.environ["MSG"])[-1])
+b = rb[1] if len(rb) > 1 else rb[0]
+open("./resources/update-log.txt", "w").write(b)
+print(json.loads(open('./package.json').read())['version'])
